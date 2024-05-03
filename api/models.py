b547499f1b041eedc170721cdf8771a741fb620b
sumeetra12 <sumeetramhrjn@gmail.com>
2024-05-03 21:10:01 +0545

    created employee table and establish the relation between company and employee table, activated admin

--- api/models.py
+++ api/models.py
@@ -12,4 +12,21 @@ class Company(models.Model):
     added_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name + '--' + self.location
+
+
 # Employee models
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=10)
+    about = models.TextField()
+    position = models.CharField(max_length=50, choices=(
+        ('Manager', 'manager'),
+        ('Software Developer', 'sd'),
+        ('Project Leader', 'pl')
+    ))
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CompanyViewSet
+from api.views import CompanyViewSet, EmployeeViewSet
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
 router.register(r'companies', CompanyViewSet)
+router.register(r'employees', EmployeeViewSet)
 
 
 urlpatterns = [
@@ -13,3 +14,6 @@ urlpatterns = [
     path('', include(router.urls))
     
 ]
+
+
+# companies/{companyId}/employess
